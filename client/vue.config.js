82a37951d62fc820c42da66a270c067492894ee6
hnezado <hnezado@gmail.com>
2024-04-06 22:53:21 +0200

    Set allowedHosts to * in vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   devServer: {
-    allowedHosts: ["3.75.158.163", "3.125.183.140", "35.157.117.28"],
+    allowedHosts: "*",
   },
   transpileDependencies: true,
   css: {
