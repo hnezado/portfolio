2c0c85359e8427fa2ec5463dbbeef6371fefce84
hnezado <hnezado@gmail.com>
2024-04-06 23:01:03 +0200

    Changed vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,14 +1,17 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+module.exports = {
   devServer: {
     disableHostCheck: true,
   },
-  transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/styles/main.css";`,
-      },
-    },
-  },
-});
+};
+
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   css: {
+//     loaderOptions: {
+//       sass: {
+//         prependData: `@import "@/styles/main.css";`,
+//       },
+//     },
+//   },
+// });
