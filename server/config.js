86c8509dcb87136c5dd15070081abcd13a40b468
hnezado <hnezado@gmail.com>
2024-04-17 22:41:28 +0200

    Updated AWS SSM parameters

--- server/config.js
+++ server/config.js
@@ -1,11 +1,11 @@
-const aws = require("aws-sdk");
+const AWS = require("aws-sdk");
 
 // AWS Configuration
-aws.config.update({ region: "eu-west-3" });
-const ssm = new aws.ssm();
+AWS.config.update({ region: "eu-west-3" });
+const ssm = new AWS.SSM();
 
 const getParam = function (param) {
-  ssm.getParameter({ Name: param, WithDecryption: true }, (err, data) => {
+  ssm.getParameter({ Name: param, WithDecryption: false }, (err, data) => {
     try {
       const parameterValue = data.Parameter.Value;
       console.log("Getting parameter value. ParameterValue:", parameterValue);
