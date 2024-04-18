213de42cd59fba358c4ee1976448cb144fd99ae4
hnezado <hnezado@gmail.com>
2024-04-18 11:33:24 +0200

    test

--- server/config.js
+++ server/config.js
@@ -5,19 +5,17 @@ AWS.config.update({ region: "eu-west-3" });
 const ssm = new AWS.SSM();
 
 async function getParam(param) {
-  const result = await ssm.getParameter(
-    { Name: param, WithDecryption: false },
-    (err, data) => {
-      try {
-        const parameterValue = data.Parameter.Value;
-        console.log("Getting parameter value. ParameterValue:", parameterValue);
-        return parameterValue;
-      } catch (err) {
-        console.error("Error obtaining parameter value", err);
-      }
-    }
-  );
-  return result;
+  try {
+    const data = await ssm
+      .getParameter({ Name: param, WithDecryption: false })
+      .promise();
+    const parameterValue = data.Parameter.Value;
+    console.log("Getting parameter value. ParameterValue:", parameterValue);
+    return parameterValue;
+  } catch (err) {
+    console.error("Error obtaining parameter value", err);
+    throw err;
+  }
 }
 
 async function getConfig() {
