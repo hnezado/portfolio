2c84078252ba19dc2344fbed799276e01f8d1c27
hnezado <hnezado@gmail.com>
2025-07-15 12:36:52 +0200

    Changed ports to 3xxx/4xxx and created projects.json for prod (valid domain URLs)

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  serverUrl: "http://192.168.1.50:3100/api", // development URL
-  // serverUrl: "https://hnezado.com:3100/api", // production URL
+  serverUrl: "http://192.168.1.50:4000/api", // development URL
+  // serverUrl: "https://hnezado.com:4000/api", // production URL
 };
 
 export default config;
